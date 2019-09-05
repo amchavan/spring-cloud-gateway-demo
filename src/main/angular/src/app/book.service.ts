817a04eafe161b597ceef6ce30b411f8a10c8274
amchavan <amchavan>
2019-09-05 13:42:49 +0200

    Basic version, all requests go to local server

--- src/main/angular/src/app/book.service.ts
+++ src/main/angular/src/app/book.service.ts
@@ -10,10 +10,8 @@ export class BookService {
     }
 
     private bookServiceUrl = 'book/';
-    private otherServiceUrl = 'http://gen.lib.rus.ec/json.php?fields=title,author,id,lcc,city,publisher,year&ids=';
 
     public findUser(bookID: number) {
-        const url = (bookID < 1000) ? this.bookServiceUrl : this.otherServiceUrl;
-        return this.http.get<Book>(url + bookID);
+        return this.http.get<Book>(this.bookServiceUrl + bookID);
     }
 }
