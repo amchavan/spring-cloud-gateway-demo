e29c6fdc21b2644c76bcaef5919d13a504d03f27
amchavan <amchavan>
2019-10-17 11:26:32 +0200

    Minor changes

--- src/main/java/alma/obops/springcloud/Controller.java
+++ src/main/java/alma/obops/springcloud/Controller.java
@@ -1,13 +1,9 @@
 package alma.obops.springcloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.mvc.ProxyExchange;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 public class Controller {
